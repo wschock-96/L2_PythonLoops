a1fa72e9ff3766f8efdd0ee89ed0286eff94f39d
wschock-96 <wschock96@gmail.com>
2024-09-09 07:06:25 -0700

    Edited commit

--- DoubleScoopNestedLoops.py
+++ DoubleScoopNestedLoops.py
@@ -6,15 +6,16 @@ days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sun
 times = ["morning", "afternoon", "evening"]
 
 for i in range(len(days)):
-    days_of_week = days[i]
-    feeling_today = random.choice(moods)
-    time_of_day = random.choice(times)
-    if time_of_day:
-        print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")
-    elif time_of_day:
-        print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")
-    else:
-        print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")   
+    for j in range(len(times)):
+        days_of_week = days[i]
+        feeling_today = random.choice(moods)
+        time_of_day = times[j]
+        if time_of_day:
+            print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")
+        elif time_of_day:
+            print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")
+        else:
+            print(f"Today is {days_of_week} {time_of_day} and I am feeling {feeling_today}!")   
  
         
     
